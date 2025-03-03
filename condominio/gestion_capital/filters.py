b3ae055db002ebd9a8c3bd3313d4342129a63208
Angelo Guerrero <g.angelo@correo.unimet.edu.ve>
2025-03-02 22:04:56 -0400

    FEAT Generacion de recibos personales

--- condominio/gestion_capital/filters.py
+++ condominio/gestion_capital/filters.py
@@ -1,61 +1,95 @@
 import django_filters
-from .models import importe
-from django.db.models import Q
+from .models import Importe, Recibo, Recibo_p, Dpto
+
+# Constante para las opciones de meses (siempre serán 12)
+MONTH_CHOICES = [
+    ('01', 'Enero'),
+    ('02', 'Febrero'),
+    ('03', 'Marzo'),
+    ('04', 'Abril'),
+    ('05', 'Mayo'),
+    ('06', 'Junio'),
+    ('07', 'Julio'),
+    ('08', 'Agosto'),
+    ('09', 'Septiembre'),
+    ('10', 'Octubre'),
+    ('11', 'Noviembre'),
+    ('12', 'Diciembre'),
+]
 
 class FiltroFecha(django_filters.FilterSet):
-    # Creacion de las opciones a mostrar
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Extrae los años únicos de 'fecha_importe' de Importe y actualiza los choices
+        years = Importe.objects.dates('fecha_importe', 'year', order='ASC')
+        year_choices = [(str(date.year), str(date.year)) for date in years]
+        self.filters['agno'].extra['choices'] = year_choices
+
+    
     mes = django_filters.ChoiceFilter(
-        choices=[
-            ('01', 'Enero'),
-            ('02', 'Febrero'),
-            ('03', 'Marzo'),
-            ('04', 'Abril'),
-            ('05', 'Mayo'),
-            ('06', 'Junio'),
-            ('07', 'Julio'),
-            ('08', 'Agosto'),
-            ('09', 'Septiembre'),
-            ('10', 'Octubre'),
-            ('11', 'Noviembre'),
-            ('12', 'Diciembre'),
-        ], 
-        method='filtro_por_mes', 
+        choices=MONTH_CHOICES,
+        method='filtro_por_mes',
         label='Mes'
     )
-
+    # El año se calculará dinámicamente a partir de los registros
     agno = django_filters.ChoiceFilter(
-        choices=[
-            ('2022', '2022'),
-            ('2023', '2023'),
-            ('2024', '2024'),
-        ],
         method='filtro_por_agno',
         label='Año'
     )
-    
-    id_dpto = django_filters.ChoiceFilter(
-        choices=importe.IdDptoChoices.choices,
+    id_dpto = django_filters.ModelChoiceFilter(
+        queryset=Dpto.objects.all(),
         method='filtro_por_id_dpto',
         label='Departamento'
     )
 
     class Meta:
-        model = importe
-        # Campos sobre los que se va a filtrar
+        model = Importe
         fields = ['mes', 'agno', 'id_dpto']
 
+    
     def filtro_por_mes(self, queryset, name, value):
-        if value:
-            return queryset.filter(fecha_importe__month=value) #Filtra por la string del mes que django toma como el numero del mes. __month es una propiedad de django
-        return queryset
+        return queryset.filter(fecha_importe__month=value) if value else queryset
 
     def filtro_por_agno(self, queryset, name, value):
-
-        if value:
-            return queryset.filter(fecha_importe__year=value)
-        return queryset
+        return queryset.filter(fecha_importe__year=value) if value else queryset
 
     def filtro_por_id_dpto(self, queryset, name, value):
-        if value:
-            return queryset.filter(id_dpto=value)
-        return queryset
+        return queryset.filter(id_dpto=value) if value else queryset
+
+class HistorialRecibosFilter(django_filters.FilterSet):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Extrae los años únicos de 'fecha_importe' de Importe y actualiza los choices
+        years = Importe.objects.dates('fecha_importe', 'year', order='ASC')
+        year_choices = [(str(date.year), str(date.year)) for date in years]
+        self.filters['anio'].extra['choices'] = year_choices
+
+    mes = django_filters.ChoiceFilter(
+        choices=MONTH_CHOICES,
+        method='filtro_por_mes',
+        label='Mes'
+    )
+    # Aquí se usan choices estáticos para el año; podrías hacerlo dinámico de forma similar a FiltroFecha
+    anio = django_filters.ChoiceFilter(
+        method='filtro_por_anio',
+        label='Año'
+    )
+    id_dpto = django_filters.ModelChoiceFilter(
+        queryset=Dpto.objects.all(),
+        label='Departamento'
+    )
+    nombre_prop = django_filters.CharFilter(
+        field_name='id_dpto__asignaciones__id_prop__nombre_prop',
+        lookup_expr='icontains',
+        label='Nombre Propietario'
+    )
+
+    class Meta:
+        model = Recibo_p
+        fields = ['id_dpto', 'nombre_prop', 'mes', 'anio']
+
+    def filtro_por_mes(self, queryset, name, value):
+        return queryset.filter(id_recibo__fecha_recibo__month=value) if value else queryset
+
+    def filtro_por_anio(self, queryset, name, value):
+        return queryset.filter(id_recibo__fecha_recibo__year=value) if value else queryset
--- condominio/gestion_capital/views.py
+++ condominio/gestion_capital/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+
+from .models import Recibo_p
+from .filters import HistorialRecibosFilter
  # permite mostrar una lista de objetos del modelo
-from .models import importe
+from .models import Importe
 from .filters import FiltroFecha
 
 def gestion_capital(request):
@@ -9,10 +12,17 @@ def gestion_capital(request):
 def vista_con_filtro(request):
     filtro = FiltroFecha(request.GET)
     # Queryset inicial (todos los query)
-    queryset = importe.objects.all()
+    queryset = Importe.objects.all()
 
     if filtro.is_valid(): # Verifica que la informacion proporcionada en el form es correcta
         queryset = filtro.qs
         contexto = {'filtro': filtro, 'queryset': queryset}
 
     return render(request, 'gestion_capital/importe_list.html', contexto)
+
+def historial_recibos(request):
+    filtro = HistorialRecibosFilter(request.GET, queryset=Recibo_p.objects.all())
+    return render(request, 'gestion_capital/historial_recibos.html', {
+        'filtro': filtro,
+        'queryset': filtro.qs  # los resultados filtrados
+    })
