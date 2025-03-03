d756d4e6a553638e7670dfdd28c946092adfa6fb
Angelo Guerrero <g.angelo@correo.unimet.edu.ve>
2025-03-02 22:11:36 -0400

    Correcciones Recibos

--- condominio/gestion_capital/filters.py
+++ condominio/gestion_capital/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import Importe, Recibo, Recibo_p, Dpto
 
-# Constante para las opciones de meses (siempre serán 12)
+# Constante para los meses (choices)
 MONTH_CHOICES = [
     ('01', 'Enero'),
     ('02', 'Febrero'),
@@ -18,24 +18,26 @@ MONTH_CHOICES = [
 ]
 
 class FiltroFecha(django_filters.FilterSet):
+    # Se encarga de tomar los años de la BDD
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Extrae los años únicos de 'fecha_importe' de Importe y actualiza los choices
         years = Importe.objects.dates('fecha_importe', 'year', order='ASC')
         year_choices = [(str(date.year), str(date.year)) for date in years]
-        self.filters['agno'].extra['choices'] = year_choices
-
+        self.filters['anio'].extra['choices'] = year_choices
     
     mes = django_filters.ChoiceFilter(
         choices=MONTH_CHOICES,
         method='filtro_por_mes',
         label='Mes'
     )
-    # El año se calculará dinámicamente a partir de los registros
-    agno = django_filters.ChoiceFilter(
-        method='filtro_por_agno',
+   
+    anio = django_filters.ChoiceFilter(
+        method='filtro_por_anio',
         label='Año'
     )
+
+    # Obtenemos todos los departamentos
     id_dpto = django_filters.ModelChoiceFilter(
         queryset=Dpto.objects.all(),
         method='filtro_por_id_dpto',
@@ -44,19 +46,20 @@ class FiltroFecha(django_filters.FilterSet):
 
     class Meta:
         model = Importe
-        fields = ['mes', 'agno', 'id_dpto']
+        fields = ['mes', 'anio', 'id_dpto']
 
     
     def filtro_por_mes(self, queryset, name, value):
-        return queryset.filter(fecha_importe__month=value) if value else queryset
+        return queryset.filter(fecha_importe__month=value) if value else queryset # __ permite ingresar al valor
 
-    def filtro_por_agno(self, queryset, name, value):
+    def filtro_por_anio(self, queryset, name, value):
         return queryset.filter(fecha_importe__year=value) if value else queryset
 
     def filtro_por_id_dpto(self, queryset, name, value):
         return queryset.filter(id_dpto=value) if value else queryset
 
 class HistorialRecibosFilter(django_filters.FilterSet):
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Extrae los años únicos de 'fecha_importe' de Importe y actualiza los choices
@@ -69,7 +72,7 @@ class HistorialRecibosFilter(django_filters.FilterSet):
         method='filtro_por_mes',
         label='Mes'
     )
-    # Aquí se usan choices estáticos para el año; podrías hacerlo dinámico de forma similar a FiltroFecha
+  
     anio = django_filters.ChoiceFilter(
         method='filtro_por_anio',
         label='Año'
