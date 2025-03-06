a5060573adf3c32714fa9cd41f094fcadf6710a7
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2025-03-06 07:29:45 -0400

    Arreglos varios en la vista de recibo

--- condominio/gestion_recibo/urls.py
+++ condominio/gestion_recibo/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('presupuestos/<int:id_recibo>/', views.crear_presupuestos, name='crear_presupuestos'),
 
-    path('reporte-mensual/', views.reporte_mensual, name='reporte_mensual'),
+    path('reporte_mensual/', views.reporte_mensual, name='reporte_mensual'),
 
-    path('gestion-fondos/', views.gestion_fondos, name='gestion-fondos'),
+    path('gestion_fondos/', views.gestion_fondos, name='gestion_fondos'),
 ]
